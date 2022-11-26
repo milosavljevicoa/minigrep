b398baa57561601015299bf58284430d8ba6ef61
milosavljevicoa <milosavljevic246@gmail.com>
2022-11-26 14:40:11 +0100

    minigrep tutorial, chapter finished

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+
+use rust_grep_tutorial::{Config, run};
+
+fn main() {
+    // --snip--
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parssing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
